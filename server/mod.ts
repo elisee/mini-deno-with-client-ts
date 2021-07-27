a0b34d80572853a92ead8a76af96ba352d2f237d
Elisée Maurer <elisee@sparklinlabs.com>
2021-07-27 23:35:20 +0200

    Don't read static files synchronously

--- server/mod.ts
+++ server/mod.ts
@@ -6,22 +6,24 @@ const server = http.serve({ port: 8080 });
 
 const publicPath = path.resolve(path.dirname(path.fromFileUrl(import.meta.url)), "../public");
 
-for await (const req of server) {
+for await (const req of server) handleReq(req);
+
+async function handleReq(req: http.ServerRequest) {
   let path = req.url;
 
   // Protect against directory traversal attacks
-  if (path.includes("..")) { req.respond({ status: 400 }); continue; }
+  if (path.includes("..")) { return await req.respond({ status: 400 }); }
 
   // Serve index by default
   if (path === "/") path = "/index.html";
 
-  let body;
-  try { body = Deno.readTextFileSync(publicPath + path); }
-  catch { req.respond({ status: 404, body: "Not found." }); continue; }
-
-  // Use Sucrase to remove type annotations on the fly to turn TypeScript into JavaScript
+  let body: Deno.Reader | string;
   if (path.endsWith(".ts")) {
-    body = sucrase.transform(body, { transforms: ["typescript"] }).code;
+    // Use Sucrase to remove type annotations on the fly to turn TypeScript into JavaScript
+    body = sucrase.transform(await Deno.readTextFile(publicPath + path), { transforms: ["typescript"] }).code;
+  } else {
+    try { body = await Deno.open(publicPath + path); }
+    catch { return await req.respond({ status: 404, body: "Not found." }); }
   }
 
   let contentType = "";
@@ -30,5 +32,5 @@ for await (const req of server) {
   else if (path.endsWith(".ts")) contentType = "text/javascript";
   // TODO: Add more media types for each extension as needed
 
-  req.respond({ headers: new Headers({ "content-type": contentType }), body: body });
+  return await req.respond({ headers: new Headers({ "content-type": contentType }), body: body });
 }
