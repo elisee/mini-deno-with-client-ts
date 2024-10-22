d44cad3c1b490f035ee36911317d565c5c5cee08
Elisée Maurer <elisee@sparklinlabs.com>
2024-10-22 17:26:30 +0200

    Update for Deno 2

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Minimal Deno with client-side TypeScript
 
-This is a minimal [Deno](https://deno.land) server example that lets you use TypeScript for client-side code too.
+This is a minimal [Deno 2](https://deno.land) server example that lets you use TypeScript for client-side code too.
 
 No bundling or extra compilation required, TypeScript files are transpiled to JavaScript on-the-fly using [Sucrase](https://sucrase.io).
+
+In production, you'll want to do some caching to avoid reloading the files from disk for each request. You can either add a simple map that keeps the bytes in memory for each URL once they've been loaded once, or put your app behind some kind of proxy server like Cloudflare.
+
+## Running the server
+
+    deno -A server/mod.ts
--- server/mod.ts
+++ server/mod.ts
@@ -1,38 +1,57 @@
-import * as http from "https://deno.land/std@0.103.0/http/mod.ts";
-import * as path from "https://deno.land/std@0.103.0/path/mod.ts";
-import * as sucrase from "https://jspm.dev/sucrase@3.20.0";
+import * as fs from "jsr:@std/fs";
+import * as path from "jsr:@std/path";
+import * as sucrase from "https://esm.sh/sucrase@3.35.0";
 
-const publicPath = path.resolve(path.dirname(path.fromFileUrl(import.meta.url)), "../public");
-const textDecoder = new TextDecoder();
+const utf8Encoder = new TextEncoder();
 
-const server = http.serve({ port: 8080 });
+Deno.serve({ port: 8080 }, handleReq);
 
-for await (const req of server) handleReq(req);
+async function handleReq(req: Request) {
+  switch (req.method) {
+    case "GET": return await handleStaticReq(req);
+    default: return new Response(null, { status: 404 });
+  }
+}
 
-async function handleReq(req: http.ServerRequest) {
-  let relPath = req.url;
-
-  // Protect against directory traversal attacks
-  if (relPath.includes("..")) { return await req.respond({ status: 400 }); }
-
-  // Serve index by default
-  if (relPath === "/") relPath = "/index.html";
-
-  let body: Deno.Reader | string;
-  try { body = await Deno.open(publicPath + relPath); }
-  catch { return await req.respond({ status: 404, body: "Not found." }); }
-
-  if (relPath.endsWith(".ts")) {
-    // Use Sucrase to remove type annotations on the fly to turn TypeScript into JavaScript
-    const tsCode = textDecoder.decode(await Deno.readAll(body));
-    body = sucrase.transform(tsCode, { transforms: ["typescript"] }).code;
+async function handleStaticReq(req: Request) {
+  let pathname = new URL(req.url).pathname;
+  if (pathname.endsWith("/")) pathname += "index.html";
+  else if (!pathname.includes(".")) {
+    const localUrl = new URL(path.join("../public", pathname, "/index.html"), import.meta.url);
+    if (await fs.exists(localUrl)) return Response.redirect(new URL(req.url + "/"));
   }
 
-  let contentType = "";
-  if (relPath.endsWith(".html")) contentType = "text/html";
-  else if (relPath.endsWith(".js")) contentType = "text/javascript";
-  else if (relPath.endsWith(".ts")) contentType = "text/javascript";
-  // TODO: Add more media types for each extension as needed
+  const localUrl = new URL(path.join("../public", pathname), import.meta.url);
 
-  return await req.respond({ headers: new Headers({ "content-type": contentType }), body: body });
+  let body: ReadableStream<Uint8Array> | Uint8Array;
+  if (pathname.endsWith(".ts")) {
+    let code;
+    try { code = sucrase.transform(await Deno.readTextFile(localUrl), { disableESTransforms: true, transforms: ["typescript"], filePath: pathname }).code; }
+    catch (err) {
+      if (err instanceof Deno.errors.NotFound) return new Response(null, { status: 404 });
+      return new Response("Failed to transpile TypeScript", { status: 500 });
+    }
+    body = utf8Encoder.encode(code);
+  } else {
+    try { body = (await Deno.open(localUrl)).readable; }
+    catch { return new Response(null, { status: 404 }); }
+  }
+
+  return new Response(body, { headers: new Headers({ "Content-Type": getContentType(pathname) }) });
+}
+
+function getContentType(pathname: string) {
+  if (pathname.endsWith(".html")) return "text/html; charset=utf-8";
+  else if (pathname.endsWith(".css")) return "text/css; charset=utf-8";
+  else if (pathname.endsWith(".ts")) return "text/javascript; charset=utf-8";
+  else if (pathname.endsWith(".js")) return "text/javascript; charset=utf-8";
+  else if (pathname.endsWith(".json")) return "application/json; charset=utf-8";
+  else if (pathname.endsWith(".webmanifest")) return "application/manifest+json; charset=utf-8";
+  else if (pathname.endsWith(".png")) return "image/png";
+  else if (pathname.endsWith(".jpg")) return "image/jpeg";
+  else if (pathname.endsWith(".svg")) return "image/svg+xml";
+  else if (pathname.endsWith(".ico")) return "image/x-icon";
+  else if (pathname.endsWith(".txt")) return "text/plain";
+
+  return "application/octet-stream";
 }
